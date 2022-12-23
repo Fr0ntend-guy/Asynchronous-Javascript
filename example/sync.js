eb5472225ba739b1de740330ceabb18c94a49b24
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-23 15:40:52 +0530

    first-commit

---
+++ example/sync.js
@@ -0,0 +1,13 @@
+console.log(" I ");
+
+console.log(" eat ");
+
+console.log(" ice cream ");
+
+console.log(" with ");
+
+console.log(" spoon ");
+
+// javaScript runs from the top to the bottom one by one serially,
+// if anyone get stuck the entire process get stuck
+// and the synchronous is same
