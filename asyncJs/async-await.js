dae84b8cae89abab1f6a6cf7327f3d4847b450a5
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-23 22:55:40 +0530

    initialize await,try,catch

---
+++ asyncJs/async-await.js
@@ -0,0 +1,39 @@
+// need to understand:
+// how to use try and catch keyword
+// how to use await keyword
+
+let stocks = {
+  Fruits: ["strawberry", "grapes", "banana", "apple"],
+  Liquid: ["water", "ice"],
+  Holder: ["cone", "cup", "stick"],
+  Toppings: ["chocolate", "peanuts"],
+};
+
+let is_shop_open = true;
+
+function toppings_choice() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(console.log("which topping would you love?"));
+    }, 3000);
+  });
+}
+
+async function kitchen() {
+  console.log("A");
+  console.log("B");
+  console.log("C");
+
+  await toppings_choice();
+
+  console.log("D");
+  console.log("E");
+}
+
+// Trigger the function
+
+kitchen();
+
+console.log("doing the dishes");
+console.log("cleaning the tables");
+console.log("taking orders");
